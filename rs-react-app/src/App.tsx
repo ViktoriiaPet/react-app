f56bdb72014e8f45f5e8bdeec26abb596516da3d
ViktoriiaPet <viktoriiap0205199@gmail.com>
2025-07-13 18:50:28 +0200

    Delete rs-react-app directory
    fix: delete files "rs-react-app"

--- rs-react-app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Component} from "react";
-
-import SearchPage from './pages/Searching.js'
-
-export default class App extends Component {
-    render() {
-        return <SearchPage />
-    }
-}
