e56192583910eaa74cddd6a0677c38d8ead170ce
Viktoriia Pet <viktoriiap0205199@gmail.com>
2025-07-18 19:18:58 +0200

    feat: add test for searching and enter the text

---
+++ rs-react-app/src/components/SearchBlock.test.tsx
@@ -0,0 +1,52 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { SearchingBlock } from './SearchBlock';
+import { vi } from 'vitest';
+
+import * as service from '../servicios/getPokeList';
+
+describe('SearchingPoke', ()=> {
+    it('should call onResult with valid data', async() => {
+        const mockData = {
+                  name: 'pikachu',
+      id: 25,
+      weight: 60,
+      sprites: { front_default: 'url_to_sprite' },
+        }
+        vi.spyOn(service, 'getData').mockResolvedValue(mockData);
+        const onResult = vi.fn();
+        render(<SearchingBlock onResult={onResult} />)
+
+        const input = screen.getByPlaceholderText(/please, enter/i)
+        const button = screen.getByRole('button', { name: /search/i })
+
+        fireEvent.change(input, {target: {value: 'pikachu'}})
+        fireEvent.click(button);
+
+      await waitFor(() => {
+      expect(onResult).toHaveBeenCalledWith(mockData);
+    });
+    })
+
+    it('should handle error if getData throws', async () => {
+    const error = new Error('Network fail');
+    const consoleSpy = vi.spyOn(console, 'error').mockImplementation(() => {});
+    vi.spyOn(service, 'getData').mockRejectedValue(error);
+
+    const onResult = vi.fn();
+
+    render(<SearchingBlock onResult={onResult} />);
+
+    const input = screen.getByPlaceholderText(/please, enter/i);
+    const button = screen.getByRole('button', { name: /search/i });
+
+    fireEvent.change(input, { target: { value: 'missingno' } });
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(consoleSpy).toHaveBeenCalledWith(error);
+      expect(onResult).not.toHaveBeenCalled();
+    });
+
+    consoleSpy.mockRestore();
+  });
+})
