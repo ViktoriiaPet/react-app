82cce1a6b9deb04b17022592200b7277b4f27a44
Viktoriia Pet <viktoriiap0205199@gmail.com>
2025-08-17 15:38:11 +0200

    fix: update tests

--- src/pages/About.test.tsx
+++ src/pages/About.test.tsx
@@ -1,4 +1,4 @@
-import { AboutPage } from '../app/[locale]/about/aboutPage';
+import AboutPage from '../app/[locale]/about/aboutPage';
 import { render, screen } from '@testing-library/react';
 
 describe('AboutPage', () => {
